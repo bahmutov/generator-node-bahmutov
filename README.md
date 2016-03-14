4b3e6d2036d4eaa18bceba05692644f89b54c678
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-03-13 20:05:31 -0400

    running first and second methods

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ Goal: replace the questions and answers from `npm init` with values collected fr
 the environment.
 
 Following [the instructions](http://yeoman.io/authoring/index.html)
+
+## Test
+
+Change into an empty folder, for example
+
+    cd /tmp
+    mkdir test-generator
+    cd test-generator
+    yo node-bahmutov
--- app/index.js
+++ app/index.js
@@ -1,5 +1,12 @@
 const generators = require('yeoman-generator')
-const g = generators.Base.extend()
+const g = generators.Base.extend({
+  method1: function () {
+    console.log('method1 running')
+  },
+  method2: function () {
+    console.log('method2 running')
+  }
+})
 console.log('extended generator')
 console.log(g)
 module.exports = g
