45f4af31866ac0f25063ad88d1156fe6754ea84b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-03-15 18:42:25 -0400

    chore(git): checking if there is current git directory

--- app/index.js
+++ app/index.js
@@ -7,12 +7,50 @@ const _ = require('lodash')
 const defaults = {
   version: '1.0.0',
   main: 'index.js',
-  license: 'MIT'
+  license: 'MIT',
+  files: [
+    'index.js',
+    'src/*.js',
+    '!src/*-spec.js'
+  ],
+  scripts: {
+    test: 'npm run unit',
+    unit: 'mocha index.js src/*.js',
+    pretest: 'npm run format && npm run lint',
+    lint: 'standard --verbose index.js src/*.js',
+    format: 'standard-format -w index.js src/*.js',
+    size: 't=\"$(npm pack .)\"; wc -c \"${t}\"; tar tvf \"${t}\"; rm \"${t}\";'
+  },
+  config: {
+    'pre-git': {
+      'commit-msg': 'simple',
+      'pre-commit': ['npm test'],
+      'pre-push': [
+        'npm run size'
+      ],
+      'post-commit': [],
+      'post-merge': []
+    }
+  }
 }
 
 const g = generators.Base.extend({
-  gitSetting: function () {
-    console.log('the git email', this.user.git.email())
+  setDefaults: function () {
+    this.answers = defaults
+  },
+  git: function git () {
+    const exists = require('fs').existsSync
+    if (!exists('.git')) {
+      console.error('Cannot find .git folder, please initialize the Git repo first')
+      console.error('git init')
+      console.error('git remote add origin ...')
+      process.exit(-1)
+    }
+  },
+  author: function author () {
+    this.answers = _.extend(this.answers, {
+      author: this.user.git.name() + ' <' + this.user.git.email() + '>'
+    })
   },
   projectName: function () {
     const done = this.async()
@@ -32,6 +70,15 @@ const g = generators.Base.extend({
       this.answers = _.extend(defaults, answers)
       debug('extended answers', this.answers)
       done()
+    })
+  },
+  repo: function repo () {
+    this.answers = _.extend(this.answers, {
+      repository: {
+        type: 'git',
+        url: 'git+https://github.com/' + this.user.github.username(this.user.git.name()) +
+          '/' + this.answers.name + '.git'
+      }
     })
   },
   installDeps: function () {
