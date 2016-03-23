143d316b87537242f7b38996a8fa98c5fdea6820
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-03-23 14:07:16 -0400

    chore(test): added end 2 end test script, closes #8

---
+++ test/e2e.sh
@@ -0,0 +1,33 @@
+echo "Linking current generator package"
+npm link
+
+echo "Creating test folder"
+folder=/tmp/test-node-generator
+rm -rf $folder
+mkdir $folder
+cd $folder
+echo "Created test folder $folder"
+
+echo "Creating local git repo"
+git init
+git remote add origin git@github.com:bahmutov/test-node-generator.git
+
+echo "Running yeoman"
+yo node-bahmutov --force <<EOF
+test-project
+this is my project
+key word1, another one
+EOF
+
+echo "Generator is done, adding a few empty files"
+touch index.js
+mkdir src
+cd src
+touch foo.js
+cd ..
+git add index.js
+git commit -m "chore(test): this is a test commit"
+
+ls -la
+git log --oneline
+echo "All done testing generator"
