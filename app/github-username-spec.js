a059cc451042c52ab9469b3181168bf78f9dde99
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-03-28 11:03:32 -0400

    fix(parse): robust ssh parsing

--- app/github-username-spec.js
+++ app/github-username-spec.js
@@ -9,8 +9,15 @@ describe('github username', () => {
     la(is.fn(getUsername))
   })
 
-  it('gets user', () => {
-    const user = getUsername('git@github.com:bahmutov/test-node-generator.git')
-    la(user === 'bahmutov', 'wrong user', user)
+  it('gets user from SSH url', () => {
+    const url = 'git@github.com:bahmutov/test-node-generator.git'
+    const user = getUsername(url)
+    la(user === 'bahmutov', 'wrong user', user, 'from', url)
+  })
+
+  it('gets user from HTTPS url', () => {
+    const url = 'https://github.com/bahmutov/test-node-generator.git'
+    const user = getUsername(url)
+    la(user === 'bahmutov', 'wrong user', user, 'from', url)
   })
 })
--- app/github-username.js
+++ app/github-username.js
@@ -1,15 +1,15 @@
 const la = require('lazy-ass')
 const is = require('check-more-types')
+const parse = require('@bahmutov/parse-github-repo-url')
 
 // parses url like git@github.com:bahmutov/test-node-generator.git
 // to get the username
 function usernameFromGithubUrl (url) {
   la(is.unemptyString(url), 'expected url string', url)
-  const matches = /github\.com:(\w+)\//.exec(url)
-  if (!matches) {
-    return
-  }
-  return matches[1]
+
+  const parsed = parse(url)
+  la(is.array(parsed), 'could not parse github url', url)
+  return parsed[0]
 }
 
 module.exports = usernameFromGithubUrl
