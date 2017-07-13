84a79630806b64562c024727c09bf8a1ba55eefa
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-07-13 10:42:33 -0400

    tried mocking https request

--- app/github-description-spec.js
+++ app/github-description-spec.js
@@ -19,4 +19,36 @@ describe.only('github repo description', () => {
     const url = 'https://github.com/bahmutov/test-node-generator.git'
     return snapshot(repoDescription(url))
   })
+
+  it('resolves with undefined if there is an error', () => {
+    const url = 'git@github.com:no-such-user/does-not-exist.git'
+    return repoDescription(url).then(description => {
+      la(description === undefined)
+    })
+  })
+
+  // TODO finish mocking? or find good HTTP interceptor mocking lib
+  describe.skip('https mock', () =>{
+    const sinon = require('sinon')
+    const https = require('https')
+
+    beforeEach(() => {
+      sinon.stub(https, 'request').callsArgWith(1, {
+        statusCode: 200,
+        headers: {}
+      }).returns({
+        once: () => {},
+        end: () => {}
+      })
+    })
+
+    afterEach(() => {
+      https.request.restore()
+    })
+
+    it('can mock non-existent repo', () => {
+      const url = 'git@github.com:no-such-user/does-not-exist.git'
+      return repoDescription(url).then(console.log)
+    })
+  })
 })
